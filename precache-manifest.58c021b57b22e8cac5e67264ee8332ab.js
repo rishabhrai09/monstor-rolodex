1477f99d5abc7d3de7a795e7bdd60f75558cd3ae
rishabhrai09 <rishabh8526@gmail.com>
2020-04-02 19:22:54 +0530

    Updates

---
+++ precache-manifest.58c021b57b22e8cac5e67264ee8332ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e36e3ca3f7339252c5ee080a206f97b",
+    "url": "/monstor-rolodex/index.html"
+  },
+  {
+    "revision": "e9e01070a6e05d0b3bbe",
+    "url": "/monstor-rolodex/static/css/main.c7c2701a.chunk.css"
+  },
+  {
+    "revision": "631ba68731ff2c3f6700",
+    "url": "/monstor-rolodex/static/js/2.5cb5cb57.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monstor-rolodex/static/js/2.5cb5cb57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e01070a6e05d0b3bbe",
+    "url": "/monstor-rolodex/static/js/main.124ba8c3.chunk.js"
+  },
+  {
+    "revision": "e1b46aba59a16920e827",
+    "url": "/monstor-rolodex/static/js/runtime-main.9756bb49.js"
+  }
+]);
